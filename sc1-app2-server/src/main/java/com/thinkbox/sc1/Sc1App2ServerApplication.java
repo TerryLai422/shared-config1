2edcfdf3b79cc7bf8f67e55de5af581031283a8b
TerryLai422 <35938314+TerryLai422@users.noreply.github.com>
2022-04-10 14:33:02 -0400

    add app2 package

---
+++ sc1-app2-server/src/main/java/com/thinkbox/sc1/Sc1App2ServerApplication.java
@@ -0,0 +1,13 @@
+package com.thinkbox.sc1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sc1App2ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sc1App2ServerApplication.class, args);
+	}
+
+}
